2f5993e84d8f404293d6813c6e4ed8f1b0b75e1f
- Z e r o <kimianalconera39@gmail.com>
2025-09-24 16:16:18 +0800

    update: webhook sync to db test

--- backend/app/api/webhooks/clerk/route.ts
+++ backend/app/api/webhooks/clerk/route.ts
@@ -1,17 +1,25 @@
 import { verifyWebhook } from '@clerk/nextjs/webhooks'
 import { NextRequest } from 'next/server'
+import prisma from "@/prisma/PrismaClient"
+
 
 export async function POST(req: NextRequest) {
   try {
     const evt = await verifyWebhook(req)
 
-    // Do something with payload
-    // For this guide, log payload to console
-    const { id } = evt.data
-    const eventType = evt.type
-    
     if (evt.type === 'user.created') {
-      console.log('userId:', evt.data.id)
+      const { id, username, email_addresses } = evt.data
+      console.log('New user created:', { id, username, email_addresses })
+
+      // Save user to database
+      await prisma.users.create({
+        data: {
+          clerkId: id,
+          username: username || '',
+          email: email_addresses[0]?.email_address || '',
+          bdate: new Date(0),
+        },
+      })
     }
 
     return new Response('Webhook received', { status: 200 })
