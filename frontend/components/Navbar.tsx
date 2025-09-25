9c6c2fcb6a3ce46bc10f78c0c1420746c799bf0c
- Z e r o <kimianalconera39@gmail.com>
2025-09-25 23:56:19 +0800

    update: costumer side page

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,14 +1,29 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
+import { LayoutGrid } from "lucide-react"
 
 export default function Navbar() {
     return (
-        <div className="flex bg-background/20 backdrop-blur-xs fixed justify-end w-full p-6">
-            <SignedIn>
-                <UserButton showName/>
-            </SignedIn>
-            <SignedOut>
-                <SignInButton />
-            </SignedOut>
+        <div className="flex fixed justify-end items-center w-full py-6 px-8">
+            <div className="flex bg-muted/20 backdrop-blur-xs justify-end w-content outline outline-muted rounded-full">
+                <SignedIn>
+                    <div className="flex flex-row gap-4 w-full justify-between items-center">
+                        {/* <div className="flex flex-1 px-6">
+                            <span className="flex flex-row gap-2 items-center font-bold font-mono">
+                                <LayoutGrid size={20} />
+                                <p>Home</p>
+                            </span>
+                        </div> */}
+
+                        <div className="flex justify-end bg-muted rounded-full py-3 px-3">
+                            <UserButton />
+                        </div>
+                    </div>
+                </SignedIn>
+
+                <SignedOut>
+                    <SignInButton />
+                </SignedOut>
+            </div>
         </div>
     )
 }
