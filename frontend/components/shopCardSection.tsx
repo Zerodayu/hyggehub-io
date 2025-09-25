d3a7bc8bed797cd90933a1a0d9042c35e37ba5fb
- Z e r o <kimianalconera39@gmail.com>
2025-09-26 01:27:30 +0800

    update: shop mini calendar

--- frontend/components/shopCardSection.tsx
+++ frontend/components/shopCardSection.tsx
@@ -1,11 +1,13 @@
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { CardAction } from "@/components/ui/card"
 import { Settings2, CircleAlert } from "lucide-react"
+import MiniCalendarSection from "./miniCalendarSection"
 
 export default function ShopCardSection() {
     return (
         <section className="flex-1 py-6 px-8">
-            <div className="grid w-full py-4 gap-14">
+            <div className="grid w-full py-4 gap-12">
+                <MiniCalendarSection />
                 <div>
                     <span className="flex items-center gap-2 pb-4">
                         <Settings2 size={16} />
