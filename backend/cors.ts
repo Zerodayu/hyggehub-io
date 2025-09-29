035a8faf0cd98e5bd5c7d0e1769b48819eb78716
- Z e r o <kimianalconera39@gmail.com>
2025-09-30 02:21:35 +0800

    update: cors missing headers

--- backend/cors.ts
+++ backend/cors.ts
@@ -1,6 +1,9 @@
 export function withCORS(response: Response) {
   response.headers.set('Access-Control-Allow-Origin', '*');
   response.headers.set('Access-Control-Allow-Methods', 'GET,POST,PATCH,PUT,DELETE,OPTIONS');
-  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, x-api-key');
+  response.headers.set(
+    'Access-Control-Allow-Headers',
+    'Content-Type, x-api-key, x-clerk-user-id, x-clerk-org-id'
+  );
   return response;
 }
