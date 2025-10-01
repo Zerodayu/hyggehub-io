e5d12d54b89d4deea878b5a22f6bafc3ea7a1af6
- Z e r o <kimianalconera39@gmail.com>
2025-10-01 17:04:18 +0800

    update: remove post method on orgs

--- backend/app/api/orgs/route.ts
+++ backend/app/api/orgs/route.ts
@@ -1,5 +1,5 @@
-export { POST } from "./handlers/post"; // create a shops code
-export { PUT } from "./handlers/put"; // update a shops code
+
+export { PUT } from "./handlers/put"; // update a shops code and phoneNo
 export { GET } from "./handlers/get"; // get org info and connected users
 
 import { withCORS } from "@/cors";
