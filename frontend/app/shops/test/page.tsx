63867789e83a10991e2e2fdc4135ba7c1926d2cb
Zerodayu <kimianalconera39@gmail.com>
2025-10-20 01:54:58 +0800

    update: dynamic shop routing

---
+++ frontend/app/shops/test/page.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { useOrganizationList } from '@clerk/nextjs'
+
+export const JoinedOrganizations = () => {
+  const { isLoaded, setActive, userMemberships } = useOrganizationList({
+    userMemberships: {
+      infinite: true,
+    },
+  })
+
+  if (!isLoaded) {
+    return <>Loading</>
+  }
+
+  return (
+    <>
+      <ul>
+        {userMemberships.data?.map((mem) => (
+          <li key={mem.id}>
+            <span>{mem.organization.name}</span>
+            <button onClick={() => setActive({ organization: mem.organization.id })}>Select</button>
+          </li>
+        ))}
+      </ul>
+
+      <button disabled={!userMemberships.hasNextPage} onClick={() => userMemberships.fetchNext()}>
+        Load more
+      </button>
+    </>
+  )
+}
