dd3e7b6ebc96c581f71cbef9c70733a11faf0012
Zerodayu <kimianalconera39@gmail.com>
2026-03-07 02:10:07 +0800

    update: cors access settings

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -5,11 +5,10 @@ const api = axios.create({
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
+    // "Access-Control-Allow-Origin": "*",
     "x-api-key": process.env.NEXT_PUBLIC_API_KEY || "",
     "x-vercel-protection-bypass": process.env.VERCEL_AUTOMATION_BYPASS_SECRET!,
   },
 });
 
 export default api;
-
