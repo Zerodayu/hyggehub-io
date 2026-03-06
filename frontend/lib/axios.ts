fb32fd140b94bae2724d40a6404966b22b84cd40
Zerodayu <kimianalconera39@gmail.com>
2026-03-07 01:06:19 +0800

    update: access control allow origin apply

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -1,14 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 10000,
   headers: {
-    'Content-Type': 'application/json',
-    // "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
     "x-api-key": process.env.NEXT_PUBLIC_API_KEY || "",
-    "x-vercel-protection-bypass": process.env.VERCEL_AUTOMATION_BYPASS_SECRET!
+    "x-vercel-protection-bypass": process.env.VERCEL_AUTOMATION_BYPASS_SECRET!,
   },
 });
 
 export default api;
+
